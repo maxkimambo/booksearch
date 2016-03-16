7f2dfc0c3739e6699add7e2e598703cbde36d6ec
Max Kimambo <max@kimambo.de>
2016-03-16 20:14:07 +0100

    ES book indexer

---
+++ config/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by max on 12.03.16.
+ */
+
+var config = {};
+// specify where the files are stored.
+config.folder = '/home/max/dev/book_search/books';
+config.elasticSearchHost = 'http://192.168.0.70';
+config.elasticSearchPort = 9200;
+config.timeout = 3000;
+
+module.exports = config;
---
+++ config/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by max on 12.03.16.
+ */
+var config = require('./config');
+
+module.exports = config;
